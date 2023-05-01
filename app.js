3fd102edbe4b2959f34b64d12a7c4c0be59de17a
Luis-Gut <132302333+Luis-Gut@users.noreply.github.com>
2023-05-01 17:16:33 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+
+const express = require('express');
+const app = express();
+
+const port = 3000;
+
+//Plantilla Motor//
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) =>{
+    res.render("index",{titulo: ""})
+})
+
+app.get("/contamina", (req, res) =>{
+    res.render("contamina",{titulo: "Hola WORLD"})
+})
+
+app.get("/contacto", (req, res) =>{
+    res.render("contacto",{titulo: "Contacto"})
+})
+
+app.get("/terms", (req, res) =>{
+    res.render("terms",{titulo: "Terminos y Condiciones"})
+})
+
+app.get("/importancia", (req, res) =>{
+    res.render("importancia",{titulo: "Importancia"})
+})
+
+app.get("/privacity", (req, res) =>{
+    res.render("privacity",{titulo: "Privacidad"})
+})
+
+
+
+
+app.listen(port, () => {
+    console.log("Listening on port",port)
+})
